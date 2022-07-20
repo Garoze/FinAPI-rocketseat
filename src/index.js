c6891caf52e5b89294c4b04173aeb9a23725b860
Garoze <marcelogaroze@hotmail.com>
2022-07-20 11:47:12 -0300

    feat: Create an account and verify if a CPF already exists on the db~

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { name, cpf } = req.body;
+
+  const customerExists = customers.some((customer) => customer.cpf === cpf);
+  if (customerExists) {
+     return res.status(400).send({error: "Customer already exists!"});
+  }
+
+  customers.push({ id: uuidV4(), name, cpf, statement: [] });
+
+  return res.status(201).send();
+});
 
 app.listen('3000', () => console.log(`Server running on port: 3000!`));
